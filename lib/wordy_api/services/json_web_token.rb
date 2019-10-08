060e5f06c27ad918bbdd1b9d6b82e59682703ed7
Michał <blind.x6@gmail.com>
2019-10-08 21:16:10 +0200

    Add JWT service

---
+++ lib/wordy_api/services/json_web_token.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class JsonWebToken
+  DEFAULT_EXPIRE_TIME = 86400 # 24 hours
+
+  class << self
+    def encode(payload, exp = Time.new + DEFAULT_EXPIRE_TIME)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, ENV['WEB_SESSIONS_SECRET'])
+    end
+
+    def decode(token)
+      JWT.decode(token, ENV['WEB_SESSIONS_SECRET']).first
+    rescue StandardError
+      nil
+    end
+  end
+end
