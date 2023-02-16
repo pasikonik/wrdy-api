11fe4795fdef355971e0f802b402d77be5e1a2f8
Michał Olejniczak <blind.x6@gmail.com>
2023-02-16 20:16:44 +0100

    Cleanup list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,19 +1,16 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :update, :destroy]
+  before_action :set_list, only: %i[show update destroy]
 
-  # GET /lists
   def index
     @lists = current_user.lists
 
     render json: @lists
   end
 
-  # GET /lists/1
   def show
     render json: @list
   end
 
-  # POST /lists
   def create
     @list = List.new(list_params.merge(user_id: current_user.id))
 
@@ -24,7 +21,6 @@ class ListsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lists/1
   def update
     if @list.update(list_params)
       render json: @list
@@ -33,19 +29,16 @@ class ListsController < ApplicationController
     end
   end
 
-  # DELETE /lists/1
   def destroy
     @list.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_list
     @list = List.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def list_params
     params.permit(:name)
   end
