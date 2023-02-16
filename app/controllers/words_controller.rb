b77b80f5870f4bf8e104b86c964c812f977f9f27
Michał Olejniczak <blind.x6@gmail.com>
2023-02-16 20:17:02 +0100

    Cleanup word controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,19 +1,6 @@
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :update, :destroy]
+  before_action :set_word, only: [:destroy]
 
-  # GET /words
-  def index
-    @words = Word.all
-
-    render json: @words
-  end
-
-  # GET /words/1
-  def show
-    render json: @word
-  end
-
-  # POST /words
   def create
     @word = Word.new(word_params)
 
@@ -24,28 +11,17 @@ class WordsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /words/1
-  def update
-    if @word.update(word_params)
-      render json: @word
-    else
-      render json: @word.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /words/1
   def destroy
     @word.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_word
-      @word = Word.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def word_params
-      params.require(:word).permit(:origin, :translation, :proficiency, :language, :list_id)
-    end
+  def set_word
+    @word = Word.find(params[:id])
+  end
+
+  def word_params
+    params.require(:word).permit(:origin, :translation, :proficiency, :language, :list_id)
+  end
 end
