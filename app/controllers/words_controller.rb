1bf4ca356012d80888b3d696b5ab0b6fab258455
Michał Olejniczak <blind.x6@gmail.com>
2023-02-28 21:47:27 +0100

    Clean up words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,29 +1,23 @@
 # frozen_string_literal: true
 
 class WordsController < ApplicationController
-  before_action :set_word, only: [:destroy]
-
   def create
-    @word = Word.new(word_params)
+    word = Word.new(word_params)
 
-    if @word.save
-      render json: @word, status: :created, location: @word
+    if word.save
+      render json: word, status: :created, location: word
     else
-      render json: @word.errors, status: :unprocessable_entity
+      render json: word.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @word.destroy
+    Word.find(params[:id]).destroy
   end
 
   private
 
-  def set_word
-    @word = Word.find(params[:id])
-  end
-
   def word_params
-    params.require(:word).permit(:origin, :translation, :proficiency, :language, :list_id)
+    params.require(:word).permit(:origin, :translation, :list_id)
   end
 end
